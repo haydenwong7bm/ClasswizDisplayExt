e6774f860d7ac2ee8570bf6726b28554ec907c5c
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2025-02-04 18:58:28 +0800

    Fixed script.

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 from sys import argv
+from os import mkdir
 import fontforge
 
 font_list = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd', 'ClassWizCWMathExt-Regular.sfd', 'ClassWizXMathExt-Regular.sfd']
 
+mkdir('output')
+
 for file_in in font_list:
     font = fontforge.open(file_in)
-    for font_out in ['.otf', '.ttf', '.woff2']:
-        font.generate(file_in.split('.')[0] + file_out)
+    for filetype in ['.otf', '.ttf', '.woff2']:
+        font.generate('output/' + file_in.split('.')[0] + filetype)
