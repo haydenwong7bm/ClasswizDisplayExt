a4d7ad9ee9c5ca3198251c941619430e4ff9b561
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2025-02-26 22:03:40 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,10 +1,10 @@
 from sys import argv
-from os import mkdir
+from os import makedirs
 import fontforge
 
 font_list = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd', 'ClassWizCWMathExt-Regular.sfd', 'ClassWizXMathExt-Regular.sfd']
 
-mkdir('output')
+makedirs('output', exist_ok=True)
 
 for file_in in font_list:
     font = fontforge.open(file_in)
