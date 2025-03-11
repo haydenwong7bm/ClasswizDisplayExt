5f0d433cb498a3177fbd47d547dba9f164e463d7
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2025-03-11 23:14:06 +0800

    It will no longer build TTF and WOFF2 for Math font.

--- build.py
+++ build.py
@@ -2,11 +2,16 @@ from sys import argv
 from os import makedirs
 import fontforge
 
-font_list = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd', 'ClassWizCWMathExt-Regular.sfd', 'ClassWizXMathExt-Regular.sfd']
+font_list_display = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd']
+font_list_math = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd']
 
 makedirs('output', exist_ok=True)
 
-for file_in in font_list:
+for file_in in font_list_display:
     font = fontforge.open(file_in)
     for filetype in ['.otf', '.ttf', '.woff2']:
         font.generate('output/' + file_in.split('.')[0] + filetype)
+
+for file_in in font_list_math:
+    font = fontforge.open(file_in)
+    font.generate('output/' + file_in.split('.')[0] + 'otf')
