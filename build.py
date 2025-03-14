2da93ccc972bdfc496b1ddf5e179839b76f1152b
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2025-03-14 23:29:34 +0800

    Fixed build issues.

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from os import makedirs
 import fontforge
 
 font_list_display = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd']
-font_list_math = ['ClassWizCWDisplayExt-Regular.sfd', 'ClassWizXDisplayExt-Regular.sfd']
+font_list_math = ['ClassWizCWMathExt-Regular.sfd', 'ClassWizXMathExt-Regular.sfd']
 
 makedirs('output', exist_ok=True)
 
@@ -14,4 +14,4 @@ for file_in in font_list_display:
 
 for file_in in font_list_math:
     font = fontforge.open(file_in)
-    font.generate('output/' + file_in.split('.')[0] + 'otf')
+    font.generate('output/' + file_in.split('.')[0] + '.otf')
