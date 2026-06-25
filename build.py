668671eac3e0728ae5239732098e95698883e1ba
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2026-06-25 22:37:46 +0800

    Update build.py

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import itertools, fontforge
 
 font_types = ['Display', 'Math']
 font_styles = ['X', 'CW']
-font_sizes = [''] # ['', 'Small', 'Tiny']
+font_sizes = ['', 'Small', 'Tiny']
 
 font_list_display = [f'ClassWiz{font_style}DisplayExt{font_size}.sfd' for font_style, font_size in itertools.product(font_styles, font_sizes)]
 font_list_math = [f'ClassWiz{font_style}DisplayExt.sfd' for font_style in font_styles]
